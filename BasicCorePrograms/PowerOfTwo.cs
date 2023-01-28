d061e27b7515bea1f954b87a19eae14d3a2a9ab9
7007singh <shw12021982@gmail.com>
2023-01-28 21:55:00 +0530

    [Shweta]Added: Table of Power of Two

--- BasicCorePrograms/PowerOfTwo.cs
+++ BasicCorePrograms/PowerOfTwo.cs
@@ -7,8 +7,9 @@ namespace BasicCorePrograms
         public static void TableOfPowerOfTwo()
         {
             Console.WriteLine("Enter a number for printing power2 table");
-            int n = Convert.ToInt32(Console.ReadLine());
-            for(int i = 0; i <= n; i++)
+            //int n = Convert.ToInt32(Console.ReadLine());
+            int n = 31;
+            for(int i = 0; i < n; i++)
             {
                 int number = (int)(double)Math.Pow(2, i);
                 Console.WriteLine("2^{0}={1}",i,number);
