08fd46900138b0c30e3453806d6fb0899523ba4f
7007singh <shw12021982@gmail.com>
2023-01-29 11:03:07 +0530

    [Shweta]Added: Check Alphabet is Vowel or Consonant

---
+++ BasicCorePrograms/VowelConsonant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicCorePrograms
+{
+    internal class VowelConsonant
+    {
+        public static void CheckVowelOrConsonant() 
+        {
+            Console.WriteLine("Please Enter a Alphabet to Check Whether it is Vowel or Not");
+            char albhabet = Convert.ToChar(Console.ReadLine());
+            if (albhabet == 'a' || albhabet == 'e' || albhabet == 'i' || albhabet == 'O' || albhabet == 'u')
+            {
+                Console.WriteLine("{0} is a Vowel", albhabet);
+            }
+            else
+            {
+                Console.WriteLine("{0} is Consonant", albhabet);
+            }
+        }
+    }
+}
